4cbab246de314b8888f85fa1fe8737a4ee972690
jlee7123 <36389087+jlee7123@users.noreply.github.com>
2022-11-09 17:10:46 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+        <Header></Header>
+        <AppBar/>
+      <router-view/>
+  </v-app>
+</template>
+
+<script>
+import Header from './layout/Header';
+//import Footer from './layout/Footer';
+//import AppBar from './components/AppBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //AppBar,
+    Header,
+    //Footer,
+  },
+
+  data: () => ({
+
+  }),
+};
+</script>
+
+
+
+
+
